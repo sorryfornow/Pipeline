5dcd2e6d71a80b6899aadc912fc4b388001e1db6
Simon Haddad <sh@SPX-LM.local>
2023-03-27 17:09:12 +0900

    Initial commit

---
+++ src/pipeline.h
@@ -0,0 +1,12 @@
+#ifndef COMP6771_PIPELINE_H
+#define COMP6771_PIPELINE_H
+
+namespace ppl {
+
+    class pipeline {
+
+    };
+
+}
+
+#endif  // COMP6771_PIPELINE_H
---
+++ src/pipeline.test.cpp
@@ -0,0 +1,7 @@
+#include "./pipeline.h"
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("Ass3 -- it's piping hot") {
+    CHECK(false);
+}
